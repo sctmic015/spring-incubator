82a1139619d9b5e43b53deebbcf2698a856b57eb
Michael Scott <michael.scott@entelect.co,za>
2023-03-14 16:20:41 +0200

    Working I think. Do I care. No I don't

--- booking-service/src/main/java/entelect/training/incubator/spring/booking/BookingServiceApplication.java
+++ booking-service/src/main/java/entelect/training/incubator/spring/booking/BookingServiceApplication.java
@@ -2,11 +2,24 @@ package entelect.training.incubator.spring.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import entelect.training.incubator.spring.notification.sms.client.impl.MoloCellSmsClient;
 
 @SpringBootApplication
 public class BookingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookingServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public MoloCellSmsClient getMoloCellSmsClient() {
+        return new MoloCellSmsClient();
     }
 }
--- booking-service/src/main/java/entelect/training/incubator/spring/booking/controller/BookingController.java
+++ booking-service/src/main/java/entelect/training/incubator/spring/booking/controller/BookingController.java
@@ -2,13 +2,21 @@ package entelect.training.incubator.spring.booking.controller;
 
 import entelect.training.incubator.spring.booking.model.Booking;
 import entelect.training.incubator.spring.booking.service.BookingService;
+import entelect.training.incubator.spring.customer.model.Customer;
+import entelect.training.incubator.spring.flight.model.Flight;
+import entelect.training.incubator.spring.notification.sms.client.impl.MoloCellSmsClient;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("bookings")
@@ -18,18 +26,50 @@ public class BookingController {
 
     private final BookingService bookingService;
 
-    public BookingController(BookingService bookingService) {
+    private final RestTemplate restTemplate;
+
+    private final MoloCellSmsClient moloCellSmsClient;
+
+
+    public BookingController(BookingService bookingService, RestTemplate restTemplate, MoloCellSmsClient moloCellSmsClient) {
         this.bookingService = bookingService;
+        this.restTemplate = restTemplate;
+        this.moloCellSmsClient = moloCellSmsClient;
     }
 
     @PostMapping
     public ResponseEntity<?> createBooking(@RequestBody Booking booking) {
-        LOGGER.info("Processing booking creation request for booking={}", booking);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
-        final Booking savedBooking = bookingService.createBooking(booking);
+        String plainCreds = "admin:is_a_lie";
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+        String base64Creds = new String(base64CredsBytes);
 
-        LOGGER.trace("Booking created");
-        return new ResponseEntity<>(savedBooking, HttpStatus.CREATED);
+        headers.add("Authorization", "Basic " + base64Creds);
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        try {
+            Customer customer = restTemplate.exchange("http://localhost:8201/customers/" + booking.getCustomerId(), HttpMethod.GET, entity, Customer.class).getBody();
+            Flight flight = restTemplate.exchange("http://localhost:8202/flights/" + booking.getFlightId(), HttpMethod.GET, entity, Flight.class).getBody();
+            final Booking savedBooking  = bookingService.createBooking(booking);
+
+            //BigDecimal balance = rewardsService.getBalance(customer.getPassportNumber());
+//            balance = balance.add(BigDecimal.valueOf(100));
+//            rewardsService.updateBalance(customer.getPassportNumber(), balance);
+
+            moloCellSmsClient.sendSms(customer.getPhoneNumber(), "Molo Air: Confirming flight " + flight.getFlightNumber() +
+                    " booked for " + customer.getFirstName() + " " + customer.getFirstName() + " on " + flight.getDepartureTime() + ".");
+
+            return new ResponseEntity<>(savedBooking, HttpStatus.OK);
+        }
+        catch (HttpClientErrorException e) {
+            System.out.println(e.getStatusCode());
+            System.out.println(e.getResponseBodyAsString());
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping
